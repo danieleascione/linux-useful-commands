9b4cc86633ba1af8bd4e5c7f45c86ac4a5ae6e31
Daniele Ascione <audhumla@users.noreply.github.com>
2019-08-11 12:05:40 +0200

    Added remove_branches_but function

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# git-useful-config
-Some useful config to improve productivity
+# linux-useful-commands
+Some useful commands
+
+## Git
+### Global setup
+
+```bash
+git config --global user.email "email@example.com"
+git config --global user.name "username"
+
+# Use kdiff as merge tool
+sudo apt-get install kdiff3
+git config --global merge.tool "kdiff3"
+
+#Use vim as default editor
+git config --global core.editor "vim"
+```
+
+### Git commands
+```bash
+# Import functions
+chmod u+x functions && . ./functions
+
+# Remove all branches but master
+remove_branches_but master
+
+# Squash against another branch
+git rebase -i <other branch>
+# Squash last 2 commits in current branch
+git rebase -i HEAD~2
+
+# Take file from another branch
+git checkout <branch> -- file
+```
--- functions.sh
+++ functions.sh
@@ -1,19 +1,50 @@
 #!/bin/bash 
 
+#
+# Constant declarations
+#
+readonly INPUT_ERROR=2
+
+#
+# Shortcut function which generates an ssh key 
+# under the directory ~/.ssh/ with rsa and:
+# - ($1) a given email
+# - ($2) a given filename
+#
 generate_key() 
 {
   if [ -z "$1" ]
   then
     echo "You must specify an email as first argument"
-    exit 0
+    return INPUT_ERROR
+  fi
+
+  if [ -z "$2" ]
+  then
+    echo "You must specify the file name as second argument"
+    return INPUT_ERROR
   fi
   
-  ssh-keygen -t rsa -b 4096 -C $1
+  # Generate key with provided name
+  ssh-keygen -t rsa -b 4096 -C $1 -f ~/.ssh/$2
+
+  # Add key to ssh-agent
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/$2
 }
 
+#
+# Remove all local branches with a different name 
+# from the one provided in input ($1)
+#
+remove_branches_but() 
+{
+  if [ -z "$1" ]
+  then
+    echo "You must specify the branch to keep"
+    return INPUT_ERROR
+  fi
 
-
-
-
-
+  git branch | grep -v '*' | grep -vw "$1" | xargs git branch -D
+}
 
